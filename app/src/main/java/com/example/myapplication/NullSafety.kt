145f69f658dcdfe3583151acd575dd65acee1c2f
azmianshari <azmianshari123@gmail.com>
2024-10-11 19:19:35 +0700

    Mengecek null dalam kondisi

--- app/src/main/java/com/example/myapplication/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -6,4 +6,11 @@ fun NullSafety(){
     // Nullable has nullable string type
     var nullable: String? = "You can a null here"
     nullable = null
+
+    // Check for null in conditions
+    if(neverNull == null){
+        println("inferredNonNull is Null")
+    } else {
+        println("inferredNonNull is not Null")
+    }
 }
