51d795bec5c42061d69dfb652fddf2e5ac2037b4
azmianshari <azmianshari123@gmail.com>
2024-10-11 18:36:48 +0700

    Membuat fungsi Equals

--- app/src/main/java/com/example/myapplication/DataClass.kt
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -14,5 +14,7 @@ fun main(){
     val data = DataClass(1,"indra@gmail.com")
     println(data)
 
-    
+    // Fungsi equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data==data2) // false
 }
