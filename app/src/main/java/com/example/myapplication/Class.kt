5971364817d8b926cd13bed69c9c231258d4c2c2
azmianshari <azmianshari123@gmail.com>
2024-10-11 18:23:55 +0700

    Membuat fun main

--- app/src/main/java/com/example/myapplication/Class.kt
+++ app/src/main/java/com/example/myapplication/Class.kt
@@ -3,3 +3,16 @@ package com.example.myapplication
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main(){
+    val contact = Contact(1,"Mary@gmail.com" )
+
+    // Print the value of the property: email
+    println(contact.email)
+
+    // Update the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Prints the new value of the property: email
+    println(contact.email)
+}
