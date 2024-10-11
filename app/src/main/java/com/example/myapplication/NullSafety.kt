88864787aba39a83aab79c9cfa42480f88a758f4
azmianshari <azmianshari123@gmail.com>
2024-10-11 19:13:57 +0700

    Mengizinkan nilai null

--- app/src/main/java/com/example/myapplication/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -2,4 +2,8 @@ package com.example.myapplication
 
 fun NullSafety(){
     var neverNull: String = "This can't be null"
+
+    // Nullable has nullable string type
+    var nullable: String? = "You can a null here"
+    nullable = null
 }
